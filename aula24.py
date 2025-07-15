16e17b2c77e0395c24a4a58db83bf07b234dbe23
Pedro H.R <pedro.ors2@gmail.com>
2025-07-14 22:00:10 -0300

    Operadores lógicos

---
+++ aula24.py
@@ -0,0 +1,8 @@
+nome = input('Digite um nome: ')
+
+if 'a' in nome:
+    print('O nome digitado contém a letra "A"')
+else:
+    print('O nome digitado não possui a letra "A"')
+
+print('dro' in nome)
